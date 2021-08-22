c63db28f091195b9d373a0ac62cf1194f2242ef5
unknown <evahong94@gmail.com>
2021-08-22 21:01:53 +0900

    '210822'

---
+++ app/src/main/java/java_private/programmers.java
@@ -0,0 +1,11 @@
+package java_private;
+
+public class programmers {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int[][] arr = {{1,2,3},{2,3,4},{3,4,5}};
+    System.out.println(arr[0].length);
+  }
+
+}
